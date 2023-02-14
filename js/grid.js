f18ddca643b5307483fc48e84b86c796b1f49c9b
ISMAILOV <elmani3107@gmail.com>
2023-02-14 10:06:20 +0100

    update
    update

--- js/grid.js
+++ js/grid.js
@@ -2,14 +2,14 @@
 let grid = document.querySelector(".grid");
 let count = 0;
 for (let i = 0; i < 240; i++) {
-  let div = document.createElement("div");
-  if (count == 0) {div.setAttribute("is_left", "true")}
-  if (count == 19) {div.setAttribute("is_right", "true")}
-  grid.appendChild(div);
-  count++;
-  console.log("count " + count);
-  console.log("i " + i);
-  if (count == 20) {count = 0}
+    let div = document.createElement("div");
+    if (count == 0) {div.setAttribute("is_left", "true")}
+    if (count == 19) {div.setAttribute("is_right", "true")}
+    grid.appendChild(div);
+    count++;
+    console.log("count " + count);
+    console.log("i " + i);
+    if (count == 20) {count = 0}
 }
 
 // if we hover over a cell and click, it will tell us its position in the grid from 1-240
@@ -21,22 +21,14 @@ for (let i = 0; i < cells.length; i++) {
     });
     cells[i].addEventListener("click", function () {
         console.log("Cell number : " + currentCell);
+    });
 
+    // detect if the player is on a div and add attribute player to true, otherwise add attribute player to false
+    document.addEventListener("keydown", function() {
+        if (i === playerPos) {
+            cells[i].setAttribute("player", "true");
+        } else {
+            cells[i].removeAttribute("player");
+        }
     });
 }
-
-
-// listen for keyboard input
-document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowLeft") {
-    move("left");
-  } else if (event.key === "ArrowRight") {
-    move("right");
-  } else if (event.key === "ArrowUp") {
-    move("up");
-  } else if (event.key === "ArrowDown") {
-    move("down");
-  } else {
-    console.log("not an arrow key!");
-  }
-});
