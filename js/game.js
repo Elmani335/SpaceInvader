f202d81ea68eae6a9e4ca23dba59876f8e7ef4bc
Kevin VONG <vongkevin.c92@gmail.com>
2023-02-15 09:31:15 +0100

    game.js

--- js/game.js
+++ js/game.js
@@ -1,49 +1,58 @@
-// create a player div
-let player = document.createElement("div");
-player.classList.add("object");
-grid.appendChild(player);
+let allGrid = document.querySelectorAll(".grid div");
 
-// set the initial position of the player
-let playerPos = 229;
-player.style.left = cells[playerPos].offsetLeft + "px";
-player.style.top = cells[playerPos].offsetTop + "px";
+// console.log(allGrid.length);
+let playerPos = 230;
 
-// handle the key events
-document.onkeydown = function(event) {
-    switch (event.keyCode) {
-        case 37: // left arrow
-            if (playerPos % 20 !== 0) {
-                playerPos--;
-                player.style.left = cells[playerPos].offsetLeft + "px";
-            }
-            break;
-        case 38: // up arrow
-            if (playerPos >= 20) {
-                playerPos -= 20;
-                player.style.top = cells[playerPos].offsetTop + "px";
-            }
-            break;
-        case 39: // right arrow
-            if (playerPos % 20 !== 19) {
-                playerPos++;
-                player.style.left = cells[playerPos].offsetLeft + "px";
-            }
-            break;
-        case 40: // down arrow
-            if (playerPos <= 219) {
-                playerPos += 20;
-                player.style.top = cells[playerPos].offsetTop + "px";
-            }
-            break;
-    }
+// create/display player's ship at playerPos
+allGrid[playerPos].classList.add("object");
 
-    // add attribute "player" to the current cell
-    cells[playerPos].setAttribute("player", "true");
-
-    // remove the attribute from all other cells
-    for (let i = 0; i < cells.length; i++) {
-        if (i !== playerPos) {
-            cells[i].removeAttribute("player");
+// move player's ship
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        if (playerPos % 20 !== 0) {
+            allGrid[playerPos].classList.remove("object");
+            playerPos -= 1;
+            allGrid[playerPos].classList.add("object");
+        }
+    } else if (event.key === "ArrowRight") {
+        if (playerPos % 20 !== 19) {
+            allGrid[playerPos].classList.remove("object");
+            playerPos += 1;
+            allGrid[playerPos].classList.add("object");
+        }
+    } else if (event.key == "ArrowUp") {
+        if (playerPos - 20 >= 0) {
+            allGrid[playerPos].classList.remove("object");
+            playerPos -= 20;
+            allGrid[playerPos].classList.add("object");
+        }
+    } else if (event.key == "ArrowDown") {
+        if (playerPos + 20 < 240) {
+            allGrid[playerPos].classList.remove("object");
+            playerPos += 20;
+            allGrid[playerPos].classList.add("object");
         }
     }
-};
+});
+
+// Press space to fire bullet
+document.addEventListener("keydown", function (event) {
+    if (event.key === " ") {
+        var bulletPos = playerPos - 20;
+        allGrid[bulletPos].classList.add("bullet");
+    }
+});
+
+// Move bullet
+setInterval(function () {
+    for (let i = 0; i < 240; i++) {
+        if (allGrid[i].classList.contains("bullet")) {
+            allGrid[i].classList.remove("bullet");
+            if (i - 20 >= 0)
+                allGrid[i - 20].classList.add("bullet");
+        }
+    }
+}, 300);
+
+// Colision detection
+
