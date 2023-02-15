047420a437ae2579530d851927b9cf91b83bfd79
Kevin VONG <vongkevin.c92@gmail.com>
2023-02-15 11:44:56 +0100

    add invader

--- js/invader.js
+++ js/invader.js
@@ -1,9 +1,7 @@
 // Create 36 invaders in a 12x3 in middle of grid
-for (let i = 0; i < 36; i++) {
-    let invader = document.createElement("div");
-    invader.classList.add("invader");
-    allGrid.appendChild(invader);
-    // Set invader's position without style.left and style.top
-    invader.setAttribute("row", Math.floor(i / 12));
-    invader.setAttribute("col", i % 12);
+let i = 0;
+
+while (i < 12) {
+    cells[i].classList.add("invader");
+    i++;
 }
