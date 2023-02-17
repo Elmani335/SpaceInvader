d541977950f7b2b51b5452e1362c011e8c0e0015
ISMAILOV <elmani3107@gmail.com>
2023-02-17 15:29:43 +0100

    Test
    test

--- js/infinity.js
+++ js/infinity.js
@@ -1,18 +1,40 @@
-// 
 
+let isinf = false;   // is infinity mode on?
+let infmode = document.getElementById("inf");
+// check if the chekbox is checked or not
+if (infmode.checked) {
+    isinf = true;
+    console.log("Infinity mode is on");
+} else {
+    isinf = false;
+    console.log("Infinity mode is off");
+}
+infmode.addEventListener("change", function () {
+    if (infmode.checked) {
+        isinf = true;
+        console.log("Infinity mode is on");
+    } else {
+        isinf = false;
+        console.log("Infinity mode is off");
+    }
+} );
 
 // when there is only 1 invader left, regenerate the invaders
 let ft_invaderRegen = setInterval(function () {
     if (isinf === true) {
-        if (isstarted === true) {
-            if (alienInvaders.length === 1) {
-                alienInvaders = [];
-                for (let i = 0; i < 240; i++) {
-                    cells[i].classList.remove("invader");
+        if (alienInvaders.length === 1) {
+            let i = 0;
+            for (let j = 0; j < 3; j++) {
+                while (i < (j + 1) * 20 - 4) {
+                    cells[i].classList.add("invader");
+                    alienInvaders.push(i);
+                    i++;
                 }
-                generateInvaders();
+                i += 8;
             }
+            i += 8;
         }
     }
-}, 1000);
+} , 1000);
+
 intervalIDs.push(ft_invaderRegen);
